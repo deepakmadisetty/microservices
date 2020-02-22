5afff3f723309fad2d0e69673495eee0b0976108
Deepak <deepak.vy400@gmail.com>
2020-02-22 10:20:10 -0600

    first commit

---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+ dockerpath=deepakmadisetty/predictapp
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker tag predictapp deepakmadisetty/predictapp
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push deepakmadisetty/predictapp
